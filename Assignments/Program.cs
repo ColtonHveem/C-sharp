1f0052cc5aa3cbba7b365c44daee11fdf76e9326
Colton Hveem <105949892+ColtonHveem@users.noreply.github.com>
2022-07-07 09:09:50 -0700

    Add files via upload

---
+++ Assignments/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Quiz5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsDoubloon("Abba"));
+            Console.WriteLine(IsDoubloon("abBa"));
+            Console.WriteLine(IsDoubloon("Caucasus"));
+            Console.WriteLine(IsDoubloon("Emmett"));
+            Console.WriteLine(IsDoubloon("arraigning"));
+        }
+        static bool IsDoubloon(string s)
+        {
+            s = s.ToLower();
+            int count = 0;
+            foreach (char c in s)
+            {
+                foreach(char d in s)
+                {  
+                    if (c == d)
+                    {
+                        count = count + 1;
+                    }
+                    
+                }
+                if (count != 2)
+                {
+                    return false;
+                }
+                count = 0;
+            }
+            return true;
+        }
+    }
+}
