6d5b97a8c3154ee5b0f4e82f6efb997075fe1cc6
Colton Hveem <105949892+ColtonHveem@users.noreply.github.com>
2022-07-07 09:16:57 -0700

    Add files via upload

---
+++ Notes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ReadWrite
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo[] cDirs = new DirectoryInfo(@"c:\").GetDirectories();
+
+            using (StreamWriter sw = new StreamWriter("target.txt"))
+            {
+                int lineN = 0;
+                foreach (DirectoryInfo dir in cDirs)
+                {
+                    lineN++;
+                    sw.WriteLine($"Line {lineN} : {dir.Name}");
+                }
+            }
+
+            int linek = 0;
+            string lines = "";
+            using (StreamReader sr = new StreamReader("source.txt"))
+            {
+                while ((lines = sr.ReadLine()) != null)
+                {
+                    linek++;
+                    Console.WriteLine($"Line {linek} : {lines}");
+                }
+            }
+        }
+    }
+}
