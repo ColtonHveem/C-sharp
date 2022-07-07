77bab87a1003c8d8c83aacecf5e3d7d25ae0b713
Colton Hveem <105949892+ColtonHveem@users.noreply.github.com>
2022-07-07 09:06:15 -0700

    Add files via upload

---
+++ Assignments/Program.cs
@@ -0,0 +1,150 @@
+﻿/*
+ * CSC205 Week7 Programming Assignment #2
+ * Calculator class: Calculator.cs
+ * Source: https://docs.microsoft.com/en-us/visualstudio/get-started/csharp/tutorial-console?view=vs-2019
+ */
+
+using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; // Default value is "not-a-number" which we use if an operation, such as division, could result in an error.
+
+            // Use a switch statement to do the math.
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    // Ask the user to enter a non-zero divisor.
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                case "e":
+                    result = Math.Pow(num1, num2);
+                    break;
+                // Return text for an incorrect option entry.
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
+
+
+/*
+ * CSC205 Week7 Programming Assignment #2
+ * Tester: Program.cs
+ * Source: https://docs.microsoft.com/en-us/visualstudio/get-started/csharp/tutorial-console?view=vs-2019
+ */
+
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool endApp = false;
+            // Display title as the C# console calculator app.
+            Console.WriteLine("Console Calculator in C#\r");
+            Console.WriteLine("------------------------\n");
+
+            while (!endApp)
+            {
+                // Declare variables and set to empty.
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+                // Ask the user to type the first number.
+                Console.Write("Type a number, and then press Enter: ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                // Ask the user to type the second number.
+                Console.Write("Type another number, and then press Enter: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("This is not valid input. Please enter an integer value: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                // Ask the user to choose an operator.
+                Console.WriteLine("Choose an operator from the following list:");
+                Console.WriteLine("\ta - Add");
+                Console.WriteLine("\ts - Subtract");
+                Console.WriteLine("\tm - Multiply");
+                Console.WriteLine("\td - Divide");
+                Console.WriteLine("\te - Exponent");
+                Console.Write("Your option? ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                // Wait for the user to respond before closing.
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); // Friendly linespacing.
+            }
+            return;
+        }
+    }
+}
+
+/* 
+Summarizing Errors:
+    - When dividing by zero, the program informs the user "This operation will result in a mathematical error."
+        - This is an appropriate way to handle the issue.
+    - When entering something other than a number, the program calls on the user to input a valid enter value.
+        - After inputing non-integers, the program still runs effectively once two consectuive integers are input by the user.
+    - When prompting the program to perform an operation on two numbers other than a,s,m or d, the program informs the user "This operation will result in a mathematical error."
+        - This response is misleading, as it is not a mathematical error like the dividing by zero error mentioned above.
+        - The program should instead inform the user that the user did not input a valid operator, similar to how it informed the user they didn't input a valid integer above.
+
+Exchanging Classes:
+    - I do not believe it would be a good idea to move the user interface and error-capturing work.
+    - Whenever debugging, it is helpful to seperate what kinds of errors the program might be having. 
+    - With the current separation, it allows the programmer to know if it is an error with the mathematical caluculations or the user interface when an error occurs.
+    - Upon noticing the error, the programmer will be able to easily locate the error with the current class set up.
+ */
