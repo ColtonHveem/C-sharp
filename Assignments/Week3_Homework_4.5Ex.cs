b50289fdf095c1655d295f3a88e6594cad889975
Colton Hveem <105949892+ColtonHveem@users.noreply.github.com>
2022-07-07 08:20:04 -0700

    Add files via upload

---
+++ Assignments/Week3_Homework_4.5Ex.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Week3_Homework_4._5Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckFermat(2,3,4,5);
+        }
+        static void CheckFermat(int a, int b, int c, int n)
+        {
+            if (n > 2 && (Math.Pow(a,n) + Math.Pow(b,n) == Math.Pow(c,n))) 
+                {
+                Console.WriteLine("Holy smokes, Fermat was wrong");
+                }
+            else
+            {
+                Console.WriteLine("No that doesn't work.");
+            }
+        }
+    }
+}
