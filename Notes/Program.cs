94ea610e14de18e785fd9620de88b975083779e0
Colton Hveem <105949892+ColtonHveem@users.noreply.github.com>
2022-07-07 08:22:18 -0700

    Add files via upload

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace Week7Notes
+namespace ConditionalsNotes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(7/3);
+            Console.WriteLine(7.0/3.0);
+            Console.WriteLine(7%3);
+            int x;
+            x = 5;
+            if (x % 2 ==0)
+            {
+                Console.WriteLine("x is even");
+            }
+            else if (x>0)
+            {
+                Console.WriteLine("x is odd");
+            }
+            else
+            {
+                Console.WriteLine("x is 0");
+            }
+            int y;
+            y = 3;
+            Console.WriteLine(Factorial(3));
+
         }
+        public static int Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+
+        }
+
     }
 }
