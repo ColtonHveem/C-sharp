1d6b0acbc6d07ce3d0ea7f7bb6073c21a620ebce
Colton Hveem <105949892+ColtonHveem@users.noreply.github.com>
2022-07-07 09:15:29 -0700

    Add files via upload

---
+++ Assignments/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class CSC205Quiz03
+{
+    static void Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+    {
+        double d = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2) + Math.Pow(z2 - z1,2));
+        Console.WriteLine(d);
+
+    }
+    public static void Main()
+    {
+        Distance(1,2,3,4,5,6);
+    }
+}
