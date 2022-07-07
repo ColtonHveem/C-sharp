8b0eb8705bcef4213ea2459eec706fc8f43ad78d
Colton Hveem <105949892+ColtonHveem@users.noreply.github.com>
2022-07-07 08:00:39 -0700

    Add files via upload

---
+++ Notes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main(string [] args) // always enter through Main first. Other lines are input into this code.
+    {
+        // arguments are decided in the black screen after the file name
+        // pressing "tab, tab" after "cw" fills the rest of the function
+        Console.WriteLine("Hello World!");
+        ThreeLines();
+        Console.Write("Hello World!\n");
+    }
+    static void NewLine()
+    {
+        Console.WriteLine("-------");
+    }
+    static void ThreeLines()
+    {
+        NewLine(); NewLine(); NewLine();
+    }
+}
+
